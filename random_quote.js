9d934b70193b8edd27ec1fb18a128b18407454a3
vikram914 <vikramkumar.official@gmail.com>
2018-02-10 13:39:51 +0530

    hello

---
+++ random_quote.js
@@ -0,0 +1,23 @@
+function getQuote() {
+   // e.preventDefault();
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+       $('#quoteTitle').text(post.title);
+        $('#quote').html(post.content);
+
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('Source:' + post.custom_meta.Source);
+        } else {
+          $('#quote-source').text('');
+        }
+      },
+      cache: false
+    });
+  }
+
+$(document).ready(getQuote);
+$('#newQuote').click(getQuote);
+
